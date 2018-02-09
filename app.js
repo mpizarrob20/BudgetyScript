665a945db771f0f975b0b54c4b07b414d802d42a
mpizarrob20 <mpizarrob20@gmail.com>
2018-02-09 18:08:18 -0300

    update class 78

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ var budgetController = (function() {
         this.value = value;        
     };
     
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        
+        data.totals[type] = sum;
+    };
+    
     var data = {
         allItems: {
             exp: [],
@@ -21,7 +30,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
     
     return {
@@ -50,6 +61,33 @@ var budgetController = (function() {
             return newItem;
         },
         
+        calculateBudget: function() {
+            
+            //Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            //Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            //Calculate the percentage of income that we spent
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
+        },
+        
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage                
+            };
+        },
+        
         testing: function() {
             console.log(data);
         }
@@ -68,7 +106,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentLabel: '.budget__expenses--percentage'
     }
     
     return {
@@ -118,6 +160,21 @@ var UIController = (function() {
             fieldsArr[0].focus();
         },
         
+        displayBudget: function(obj){
+            
+            document.querySelector(DOMstring.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstring.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstring.expenseLabel).textContent = obj.totalExp;
+            
+            
+            if(obj.percentage > 0){
+                document.querySelector(DOMstring.percentLabel).textContent = obj.percentage + '%';
+            }else {
+                document.querySelector(DOMstring.percentLabel).textContent = '---';
+            }
+            
+        },
+        
         getDOMstring: function(){
             return DOMstring;
         }
@@ -142,14 +199,16 @@ var controller = (function(budgetCtrl, UICtrl){
     };
     
     
-    var updateBudget = function(){
-        
+    var updateBudget = function(){        
                
        // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
         
        // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
        
        // 3. Display the budget on the UI
+        UICtrl.displayBudget(budget);
         
     };
     
@@ -173,15 +232,18 @@ var controller = (function(budgetCtrl, UICtrl){
             //Calculate and update budget
             updateBudget();
         }
-               
-       
-       
 
     }
     
      return {
          init: function() {
              console.log('Aplication has started');
+             UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1                
+            });
              setupEventListeners();
          }
      }
